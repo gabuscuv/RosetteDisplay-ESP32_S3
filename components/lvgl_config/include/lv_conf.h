f7636de3e57342cc0df83cdf8ac76d1d61cab000
Gabriel Bustillo del Cuvillo <dev@gabuscuv.dev>
2026-05-17 08:48:59 +0200

    Added lvgl and tjpgd dependencies

---
+++ components/lvgl_config/include/lv_conf.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define LV_COLOR_DEPTH 16
+
+#define LV_USE_LOG 1
+#define LV_USE_LABEL 1
+#define LV_USE_BTN 1
+#define LV_USE_IMG 1
+#define LV_USE_QRCODE 1
+
+#define LV_MEM_SIZE (64U * 1024U)
+
+#define LV_TICK_CUSTOM 1
